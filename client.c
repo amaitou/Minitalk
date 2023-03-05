d536d2978b045d995faeb79d15f66147ad95b95f
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-03-05 15:58:57 +0100

    added error messages

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 22:56:15 by amait-ou          #+#    #+#             */
-/*   Updated: 2022/12/31 22:58:34 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/03/05 15:49:58 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,15 @@ void	ft_client(pid_t pid, char *s)
 int	main(int ac, char **ag)
 {
 	if (ac <= 2)
+	{
+		ft_putstr("[!] Missing Arguments\n");
 		return (0);
+	}
+	if (ft_atoi(ag[1]) <= 0)
+	{
+		ft_putstr("[!] Invalid Process Id\n");
+		return (0);
+	}
 	ft_client(ft_atoi(ag[1]), ag[2]);
 	return (0);
 }
--- client_bonus.c
+++ client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 22:56:15 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/01/01 15:09:56 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/03/05 15:50:13 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,17 @@ int	main(int ac, char **ag)
 	struct sigaction	sa;
 
 	if (ac <= 2)
+	{
+		ft_putstr("[!] Missing Arguments\n");
 		return (0);
+	}
 	sa.sa_handler = &handler;
-	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR1, &sa, (void *)0);
+	if (ft_atoi(ag[1]) <= 0)
+	{
+		ft_putstr("[!] Invalid Process Id\n");
+		return (0);
+	}
 	ft_client(ft_atoi(ag[1]), ag[2]);
 	return (0);
 }
